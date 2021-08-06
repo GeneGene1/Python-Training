8d8f0b88de1036abd96600bb06e4996a83d68c9a
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2021-08-06 01:20:01 -0400

    Add files via upload

---
+++ pythonpractice.py
@@ -0,0 +1,27 @@
+# Hello world
+print('Hello world')
+# Python is interesting
+print("Python is interesting")
+# Simple Operation
+print(4+1)
+print(5+2)
+print(2*(3+4))
+# Float
+print(10/5)
+# Test
+print((23*2)+(27*2)+(18*2))
+# Float
+print(5+5.0)
+print(5+5.001)
+print(5+5.0001)
+# Exponentiation
+# Raise to the number of power
+print(2**2)
+print(2**3)
+print(8**(1/3))
+# Quotient
+# Division without float
+print(20//5)
+# Remainder
+# Number left over after division
+print(20%3)
